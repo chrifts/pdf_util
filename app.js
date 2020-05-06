adea14090e8dd76ec3b59dd14799150530988df9
Cristian <schweizercristian@gmail.com>
2020-05-06 22:19:54 +0200

    events form

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var app = express();
 var formidable = require('formidable');
-require('dotenv').config()
+var env = require('dotenv').config().parsed;
 const exec = require('child_process').exec;
+
 
 function os_func() {
     this.execCommand = function(cmd, callback) {
@@ -18,38 +19,46 @@ function os_func() {
 }
 var os = new os_func();
 
+function process(now, filePath, bookname, res) {
+    os.execCommand('pdftk '+filePath+' burst output '+__dirname + '/uploads/'+now+'/output_%02d.pdf', function (returnvalue) {
+        //ENCRYPT
+        os.execCommand('cd '+__dirname+'/uploads/'+now+'; ls | grep -v output_*.*$ | xargs rm', function (countx) {
+            os.execCommand('cd '+__dirname+'/uploads/'+now+'; ls | wc -l', function (count) {
+                console.log(count)
+                var i;
+                for (i = 1; i <= count; i++) {
+                    i < 10 ? i = '0'+i : i;
+                    os.execCommand('cd '+__dirname+'/uploads/'+now+'; qpdf --encrypt ' + env.ENCRYPTER + ' ' + env.ENCRYPTER + ' 40 -- '+__dirname + '/uploads/'+now+'/output_'+i+'.pdf '+__dirname + '/uploads/'+now+'/encrypted_'+i+'.pdf', function (returnvaluexd) {
+                    })
+                }
+               
+                os.execCommand('cd '+__dirname+'/uploads/'+now+'; zip -r '+bookname+'.zip ' + 'encrypted_*.pdf', function (returnvalue2) {
+                    console.log(returnvalue2);
+                    res.sendFile(__dirname+'/uploads/'+now+'/'+bookname+'.zip');
+                });
+            })
+        })
+        
+    });
+}
+
 app.post('/', function (req, res) {
     var now = Date.now();
-    var bookname = 'thebook'
     var form = new formidable.IncomingForm();
-
+    var route;
+    var bookname = 'thebook';
     try {
         form.parse(req, function (err, fields, files) {
-            os.execCommand('pdftk '+files.theFile.path+' burst output '+__dirname + '/uploads/'+now+'/output_%02d.pdf', function (returnvalue) {
-                //ENCRYPT
-                os.execCommand('cd '+__dirname+'/uploads/'+now+'; ls | grep -v output_*.*$ | xargs rm', function (countx) {
-                    os.execCommand('cd '+__dirname+'/uploads/'+now+'; ls | wc -l', function (count) {
-                        console.log(count)
-                        var i;
-                        for (i = 1; i <= count; i++) {
-                            i < 10 ? i = '0'+i : i;
-                            os.execCommand('cd '+__dirname+'/uploads/'+now+'; qpdf --encrypt ' + process.env.ENCRYPTER + ' ' + process.env.ENCRYPTER + ' 40 -- '+__dirname + '/uploads/'+now+'/output_'+i+'.pdf '+__dirname + '/uploads/'+now+'/encrypted_'+i+'.pdf', function (returnvaluexd) {
-                            })
-                        }
-                       
-                        os.execCommand('cd '+__dirname+'/uploads/'+now+'; zip -r '+bookname+'.zip ' + 'encrypted_*.pdf', function (returnvalue2) {
-                            console.log(returnvalue2);
-                            res.sendFile(__dirname+'/uploads/'+now+'/'+bookname+'.zip');
-                        });
-                    })
-                })
-                
-            });
+            var filepath = files.theFile.path
         });
         form.on('fileBegin', function (name, file){
             os.execCommand('mkdir uploads/'+now, function (ret) {})
             file.path = __dirname + '/uploads/'+now+'/' + file.name.replace(/ /g, '');
+            route = file.path;
         });
+        form.on('end', () => {
+            process(now, route, bookname, res);
+        })
     } catch (error) {
         res.send(error)
     }
